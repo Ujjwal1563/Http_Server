5c762d0582647df414b429bf2dbc14aa0ecbcbb5
Ujjwal Sharma <101029502+Ujjwal1563@users.noreply.github.com>
2024-06-05 16:40:52 +0530

    new

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
-const myServer= http.createServer((req, res)=>{
-    const log = `${Date.now()}: ${req.url} New Req Received\n`; 
-    fs.appendFile("log.txt", log, (err, data) => {
-        switch (req.url) {
+const myServer = http.createServer((req, res) => {
+    const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
+
+    fs.appendFile("log.txt", log, (err) => {
+        if (err) {
+            res.statusCode = 500;
+            res.end("Internal Server Error");
+            return;
+        }
+
+        switch (myUrl.pathname) {
             case '/home':
                 res.end("HomePage");
                 break;
             case '/about':
-                res.end("I am Ujjwal");
+                const username = myUrl.query.myname;
+                res.end(`Hi, ${username}`);
                 break;
+            case '/search':
+                const search = myUrl.query.search_query;
+                res.end("Here are your results for " + search);
+                break;  // Add break statement here
             default:
                 res.end("404 Not Found");
                 break;
         }
-        res.end("Hello From Server");
     });
 });
 
